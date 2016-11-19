5b40e7212dc372b4f8537240c421066b8fecc37a
Awesome123man <daugherty7654@gmail.com>
2016-11-19 07:29:51 -0500

    I will add the usual comments later i believe. The name should say it all for each method.

--- framework/UI/BreakHandler/BreakHandler.java
+++ framework/UI/BreakHandler/BreakHandler.java
@@ -1,14 +1,28 @@
 package com.AwesomeAPI.framework.UI.BreakHandler;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.runemate.game.api.client.ClientUI;
+import com.runemate.game.api.hybrid.Environment;
 import com.runemate.game.api.hybrid.util.StopWatch;
+import com.runemate.game.api.hybrid.util.calculations.Random;
+import com.runemate.game.api.script.framework.AbstractBot;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Matthew on 11/15/2016.
@@ -26,16 +40,16 @@ public class BreakHandler {
      * Used to determine if a bot is breaking or not.
      *
      * @param SbreakTimes A String array of startTimes.
-     * @param Sdurations A String array of durations.
-     * @param stopWatch A StopWatch from the main bot to use to check times.
+     * @param Sdurations  A String array of durations.
+     * @param stopWatch   A StopWatch from the main bot to use to check times.
      * @return Boolean that is true when a break contains the current StopWatch time, false if none do.
      */
-    public static boolean isBreaking(String[] SbreakTimes, String[] Sdurations, StopWatch stopWatch){
+    public static boolean isBreaking(String[] SbreakTimes, String[] Sdurations, StopWatch stopWatch) {
         boolean breaking = false;
         int breakTimes[] = new int[SbreakTimes.length];
         int durations[] = new int[Sdurations.length];
 
-        for(int i = 0; i < breakTimes.length; i++){
+        for (int i = 0; i < breakTimes.length; i++) {
             breakTimes[i] = convertToMilli(SbreakTimes[i]);
             durations[i] = convertToMilli(Sdurations[i]);
         }
@@ -59,8 +73,8 @@ public class BreakHandler {
      * @param time A use input String for a runtime in format 00:00:00.
      * @return True if the format contains the ":"s and appropriate numbers with no other characters.
      */
-    public static boolean checkValid(String time){
-        if(!time.matches("^\\d{2}:\\d{2}:\\d{2}$")){
+    public static boolean checkValid(String time) {
+        if (!time.matches("^\\d{2}:\\d{2}:\\d{2}$")) {
             ClientUI.sendTrayNotification("Time is not in correct format, Ex (00:00:00)");
             return false;
         }
@@ -70,11 +84,11 @@ public class BreakHandler {
     /**
      * Used to calculate the end time as a time String from a start and duration String.
      *
-     * @param start A String time format for the start time.
+     * @param start    A String time format for the start time.
      * @param duration A String time format for the duration of the break.
      * @return A String that is the end time for the break.
      */
-    public static String getEnd(String start, String duration){
+    public static String getEnd(String start, String duration) {
         return revertToString(convertToMilli(start) + convertToMilli(duration));
     }
 
@@ -82,10 +96,10 @@ public class BreakHandler {
      * Used to calculate the start time as a time String from a duration and end String.
      *
      * @param duration A String time format for the duration of the break.
-     * @param end A String time format for the end time of the break.
+     * @param end      A String time format for the end time of the break.
      * @return A String that is the Start time of the break.
      */
-    public static String getStart(String duration, String end){
+    public static String getStart(String duration, String end) {
         return revertToString(convertToMilli(end) - convertToMilli(duration));
     }
 
@@ -93,10 +107,10 @@ public class BreakHandler {
      * Used to get the duration of a break from the start and end time Strings.
      *
      * @param start A String time format for the start time of the break.
-     * @param end A String time format for the end of the break.
+     * @param end   A String time format for the end of the break.
      * @return A String that is the duration of the break.
      */
-    public static String getDuration(String start, String end){
+    public static String getDuration(String start, String end) {
         return revertToString(convertToMilli(end) - convertToMilli(start));
     }
 
@@ -107,21 +121,19 @@ public class BreakHandler {
      * @param runtime A String time to be converted to milliseconds.
      * @return An int that is the number of whole milliseconds the runtime represents.
      */
-    public static int convertToMilli(String runtime){
+    public static int convertToMilli(String runtime) {
         int numOfColons = 0;
         int converted = 0;
-        for (int l = 0; l < runtime.length(); l++){
-            if(runtime.charAt(l) == ':'){
+        for (int l = 0; l < runtime.length(); l++) {
+            if (runtime.charAt(l) == ':') {
                 numOfColons++;
             }
         }
-        if(numOfColons != 2){
+        if (numOfColons != 2) {
             ClientUI.sendTrayNotification("Time is not in correct format, Ex (00:00:00)");
-        }
-        else if(!checkValid(runtime)){
+        } else if (!checkValid(runtime)) {
             ClientUI.sendTrayNotification("Time is not in correct format, Ex (00:00:00)");
-        }
-        else{
+        } else {
             converted = Integer.parseInt(runtime.substring(0, runtime.indexOf(':'))) * 3600000
                     + Integer.parseInt(runtime.substring(runtime.indexOf(':') + 1, runtime.lastIndexOf(':'))) * 60000
                     + Integer.parseInt(runtime.substring(runtime.lastIndexOf(':') + 1, runtime.length())) * 1000;
@@ -170,9 +182,9 @@ public class BreakHandler {
      * An EventHandler that will fill in the different ListViews you have for your break handler.
      * I wish i knew how to use TableView :( .
      *
-     * @param LV_Start A ListView for the start times.
+     * @param LV_Start    A ListView for the start times.
      * @param LV_Duration A ListView for the Durations.
-     * @param LV_End A ListView for the ends.
+     * @param LV_End      A ListView for the ends.
      * @return An EventHandler that will execute the event whenever someone edits the ListView this is added to.
      */
     public static EventHandler<ListView.EditEvent<String>> listViewFillInTimes(ListView<String> LV_Start, ListView<String> LV_Duration, ListView<String> LV_End) {
@@ -219,39 +231,40 @@ public class BreakHandler {
     /**
      * Created by Matthew on 11/18/2016.
      */
+    //I will add commments to this later. Too tired rn
     public static class BreakTracker {
 
         private final StringProperty startTime;
         private final StringProperty duration;
         private final StringProperty endTime;
 
-        public BreakTracker(String startTime, String duration, String endTime){
+        public BreakTracker(String startTime, String duration, String endTime) {
             this.startTime = new SimpleStringProperty(startTime);
             this.endTime = new SimpleStringProperty(endTime);
             this.duration = new SimpleStringProperty(duration);
         }
 
-        public String getStartTime(){
+        public String getStartTime() {
             return startTime.get();
         }
 
-        public String getDuration(){
+        public String getDuration() {
             return duration.get();
         }
 
-        public String getEndTime(){
+        public String getEndTime() {
             return endTime.get();
         }
 
-        public void setStartTime(String time){
+        public void setStartTime(String time) {
             startTime.set(time);
         }
 
-        public void setDuration(String time){
+        public void setDuration(String time) {
             duration.set(time);
         }
 
-        public void setEndTime(String time){
+        public void setEndTime(String time) {
             endTime.set(time);
         }
 
@@ -259,11 +272,11 @@ public class BreakHandler {
             return startTime;
         }
 
-        public StringProperty durationProperty(){
+        public StringProperty durationProperty() {
             return duration;
         }
 
-        public StringProperty endProperty(){
+        public StringProperty endProperty() {
             return endTime;
         }
 
@@ -276,7 +289,7 @@ public class BreakHandler {
             int col;
             String time1;
             time = event.getNewValue();
-            if(BreakHandler.checkValid(time)) {
+            if (BreakHandler.checkValid(time)) {
                 row = event.getTablePosition().getRow();
                 System.out.println(row);
                 col = event.getTablePosition().getColumn();
@@ -304,6 +317,167 @@ public class BreakHandler {
                     } else if (!(time1 = breakTracker.get(row).durationProperty().get()).isEmpty()) {
                         breakTracker.get(row).setStartTime(BreakHandler.getStart(time1, time));
                     }
+                }
+            }
+        };
+    }
+
+    public static EventHandler<ActionEvent> generateBreaks(List<BreakTracker> breakTracker) {
+        return event -> {
+            breakTracker.clear();
+            for (int i = 0; i < 15; i++) {
+                int duration = 0;
+                int startTime = 0;
+                if (i > 0)
+                    breakTracker.add(new BreakTracker(revertToString((startTime = convertToMilli(breakTracker.get(i - 1).getEndTime()) + 6 * Random.nextInt(900000, 3600001))),
+                            revertToString((duration = 6 * Random.nextInt(450000, 3600001))), revertToString(startTime + duration)));
+                else
+                    breakTracker.add(new BreakTracker(revertToString(6 * Random.nextInt(900000, 3600001)),
+                            revertToString((duration = 6 * Random.nextInt(450000, 3600001))), revertToString(startTime + duration)));
+            }
+        };
+    }
+
+    public static EventHandler<ActionEvent> clearBreaks(List<BreakTracker> breakTracker) {
+        return event -> {
+            breakTracker.clear();
+            breakTracker.add(new BreakTracker("", "", ""));
+        };
+    }
+
+    public static EventHandler<ActionEvent> saveBreaks(List<BreakTracker> breakTracker, TextField profTF, ComboBox<String> profsBox, AbstractBot bot) {
+        return event -> {
+            String path = "";
+            String prof = profTF.getText();
+            if(prof != null) {
+                profsBox.getItems().add(prof);
+                JsonObject jBreaks = new JsonObject();
+                JsonArray listOfStarts = new JsonArray();
+                JsonArray listOfDurations = new JsonArray();
+                JsonArray listOfEnds = new JsonArray();
+                for (int i = 0; i < breakTracker.size(); i++) {
+                    listOfStarts.add(breakTracker.get(i).getStartTime());
+                }
+                for (int i = 0; i < breakTracker.size(); i++) {
+                    listOfDurations.add(breakTracker.get(i).getDuration());
+                }
+                for (int i = 0; i < breakTracker.size(); i++) {
+                    listOfEnds.add(breakTracker.get(i).getEndTime());
+                }
+
+                jBreaks.add("Start", listOfStarts);
+                jBreaks.add("Duration", listOfDurations);
+                jBreaks.add("End", listOfEnds);
+
+                System.out.println(jBreaks);
+
+                try {
+
+                    try {
+                        path = bot.getPlatform().invokeAndWait(() -> Environment.getStorageDirectory().getPath());
+                    } catch (ExecutionException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    // Writing to a file
+                    System.out.println(prof);
+                    File file = new File(path + "\\" + prof + ".json");
+                    file.createNewFile();
+                    FileWriter fileWriter = new FileWriter(file);
+                    System.out.println("Writing JSON object to file");
+                    System.out.println("-----------------------");
+
+                    fileWriter.write(jBreaks.toString());
+                    fileWriter.flush();
+                    fileWriter.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+
+    public static EventHandler<ActionEvent> loadBreaks(List<BreakTracker> breakTracker, ComboBox<String> profName, AbstractBot bot) {
+        return event -> {
+            breakTracker.clear();
+            String path = "";
+            String prof = profName.getSelectionModel().getSelectedItem();
+
+            try {
+                path = bot.getPlatform().invokeAndWait(() -> Environment.getStorageDirectory().getPath());
+
+                JsonParser parser = new JsonParser();
+
+                Object obj = parser.parse(new FileReader(path + "\\" + prof + ".json"));
+
+                JsonObject jsonObject = (JsonObject) obj;
+
+                System.out.println("Starts are :");
+                JsonArray listOfStarts = (JsonArray) jsonObject.get("Start");
+                Iterator<JsonElement> iterator = listOfStarts.iterator();
+                while (iterator.hasNext()) {
+                    System.out.println(iterator.next());
+                }
+                System.out.println("Durations are :");
+                JsonArray listOfDurations = (JsonArray) jsonObject.get("Duration");
+                Iterator<JsonElement> iteratorD = listOfDurations.iterator();
+                while (iteratorD.hasNext()) {
+                    System.out.println(iteratorD.next());
+                }
+                System.out.println("Ends are :");
+                JsonArray listOfEnds = (JsonArray) jsonObject.get("End");
+                Iterator<JsonElement> iteratorE = listOfEnds.iterator();
+                while (iteratorE.hasNext()) {
+                    System.out.println(iteratorE.next());
+                }
+
+                for(int i = 0; i < listOfStarts.size(); i++){
+                    breakTracker.add(new BreakTracker(listOfStarts.get(i).getAsString(), listOfDurations.get(i).getAsString(), listOfEnds.get(i).getAsString()));
+                }
+            } catch (IOException | InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+
+        };
+    }
+
+    public static List<String> loadProfs(AbstractBot bot) {
+        List<String> results = new ArrayList<>();
+        String path = null;
+        try {
+            path = bot.getPlatform().invokeAndWait(() -> Environment.getStorageDirectory().getPath());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        File[] files = new File(path).listFiles((dir, name) -> name.endsWith(".json"));
+
+        for (File file : files) {
+            if (file.isFile()) {
+                results.add(file.getName().replace(".json", ""));
+            }
+        }
+
+        return results;
+    }
+
+    public static EventHandler<ActionEvent> deleteProf(ComboBox<String> cb_profile, AbstractBot bot) {
+        return event -> {
+            String path = "";
+            try {
+                path = bot.getPlatform().invokeAndWait(() -> Environment.getStorageDirectory().getPath());
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+            String prof = cb_profile.getSelectionModel().getSelectedItem();
+            int index = cb_profile.getSelectionModel().getSelectedIndex();
+            if(prof != null) {
+                File file = new File(path + "\\" + prof + ".json");
+                if(file.delete()){
+                    System.out.println("Deleted Profile: " + prof);
+                    cb_profile.getItems().remove(index);
                 }
             }
         };
